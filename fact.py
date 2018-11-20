8ba39d14a1abf372f62ae5e5475851f1ffece60e
Rohit Neel <rohitneel007@gmail.com>
2018-11-20 10:50:46 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,16 +1,6 @@
-num = 407
+fact=1
 
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
-else:
-   print(num,"is not a prime number")
+for i in range(1,6):
+   fact=fact*i
+   
+print(fact)
